118d26c4137bc97b865f8825ed48e9fdf017129b
aspeedi777 <aspeedi777@gmail.com>
2021-03-14 23:41:10 +0100

    bind auth properties by pojo class AppAuthProperties

--- src/main/java/com/pl/przytula/dietapp/DietappApplication.java
+++ src/main/java/com/pl/przytula/dietapp/DietappApplication.java
@@ -1,8 +1,11 @@
 package com.pl.przytula.dietapp;
 
+import com.pl.przytula.dietapp.config.AppAuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AppAuthProperties.class)
 @SpringBootApplication
 public class DietappApplication {
 
